dde4bafb640aba9cfbe243ed65bc71d25868fac4
github username <github email address>
2017-05-08 11:03:45 -0400

    Matt

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -3,4 +3,17 @@ class EpisodesController < ApplicationController
   def index
     @episodes = Episode.all
   end
+
+  def new
+    @episode = Episode.new()
+  end
+
+  def create
+    @episode = Episode.find_or_create_by(id: params[:id])
+  end
+
+  def show
+    @episode = Episode.find_by(id: params[:id])
+  end
+
 end
--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -3,4 +3,20 @@ class GuestsController < ApplicationController
   def index
     @guests = Guest.all
   end
+
+
+
+  def new
+    @guest = Guest.new
+    @appearance = Appearance.new
+  end
+
+  def create
+    @guest = Guest.find_or_create_by(id: params[:id])
+    @appearance = Appearance.create(guest_id: params[:appearance][:id],episode_id: params[:appearance][:id],star_rating:params[:appearance][:star_rating])
+  end
+
+  def show
+    @guest = Guest.find_by(id: params[:id])
+  end
 end
